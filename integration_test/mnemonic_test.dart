059f42d7b77c74d3ccff2107a69878fef3507c38
ciripel <nedelcu.alexandru@yahoo.com>
2022-03-18 20:05:12 +0200

    fix: switched to our trust_wallet_core_lib

--- integration_test/mnemonic_test.dart
+++ integration_test/mnemonic_test.dart
@@ -1,9 +1,9 @@
 import 'package:test/test.dart';
 import 'package:sio_core/sio_core.dart';
-import 'package:trust_wallet_core/flutter_trust_wallet_core.dart' as trust_core;
+import 'package:trust_wallet_core_lib/trust_wallet_core_lib.dart' as trust_core;
 
 void main() {
-  trust_core.FlutterTrustWalletCore.init();
+  trust_core.TrustWalletCoreLib.init();
   test('Mnemonic is valid', () {
     const mnemonic =
         'rent craft script crucial item someone dream federal notice page shrug pipe young hover duty';
--- lib/src/mnemonic.dart
+++ lib/src/mnemonic.dart
@@ -1,4 +1,4 @@
-import 'package:trust_wallet_core/flutter_trust_wallet_core.dart' as trust_core;
+import 'package:trust_wallet_core_lib/trust_wallet_core_lib.dart' as trust_core;
 
 class Mnemonic {
   var wallet = trust_core.HDWallet();
