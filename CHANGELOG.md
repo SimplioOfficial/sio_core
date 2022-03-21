acfe83a1bc721c35539b2034e1f428aa3661f373
ciripel <nedelcu.alexandru@yahoo.com>
2022-03-21 16:59:08 +0200

    docs: implemented documentation & v0.0.4 close #1, close #2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,12 @@
+## 0.0.4
+
+* Implemented mnemonic operations
+## 0.0.3
+
+* Testing continuous delivery on github release
+## 0.0.2
+* Just a test for pub.dev
 ## 0.0.1
 
 * This release is a meant to be a proof of concept and to test if this can be
   installed in another flutter app.
-
-## 0.0.2
-
-* Just a test for pub.dev
-
-## 0.0.3
-
-* Testing continuous delivery on github release
--- lib/src/mnemonic.dart
+++ lib/src/mnemonic.dart
@@ -1,17 +1,22 @@
 import 'package:trust_wallet_core_lib/trust_wallet_core_lib.dart' as trust_core;
 
+///Class that manipulates BIP39 English mnemonics
 class Mnemonic {
   var wallet = trust_core.HDWallet(strength: 256);
 
+  /// Generate an 24 words valid BIP39 English mnemonic.
   String get generate {
     return wallet.mnemonic();
   }
 
-  // Supports only 12 or 24 words mnemonics
+  /// Determines whether a BIP39 English mnemonic phrase is valid.
+  /// Wallet Core supports only 12 or 24 words mnemonic phrases
   static bool isValid({required String mnemonic}) {
     return trust_core.Mnemonic.isValid(mnemonic);
   }
 
+  /// Creates an HDWallet from a valid BIP39 English mnemonic.
+  /// Null is returned on invalid mnemonic. Returned object needs to be deleted.
   static trust_core.HDWallet import({required String mnemonic}) {
     return trust_core.HDWallet.createWithMnemonic(mnemonic);
   }
