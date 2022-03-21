6ba06db89f7cb94e954475d7a797d7c7598340d2
ciripel <37701673+ciripel@users.noreply.github.com>
2022-03-21 16:17:57 +0200

    subtask: implemented mnemonic operation (#12)
    close #9, close #10

--- integration_test/on_emulator_test.dart
+++ integration_test/on_emulator_test.dart
@@ -6,22 +6,33 @@ void main() {
   trust_core.TrustWalletCoreLib.init();
 
   group('Mnemonic tests', () {
-    test('Mnemonic is valid', () {
-      const mnemonic =
-          'rent craft script crucial item someone dream federal notice page shrug pipe young hover duty';
-      expect(Mnemonic().isValid(mnemonic: mnemonic), equals(true));
+    const mnemonic12Words =
+        'rent craft script crucial item someone dream federal notice page shrug pipe young hover duty';
+    const mnemonic24Words =
+        'trouble sand fringe fox spatial film attract domain biology exchange twist audit nose raccoon steel warrior arrest nut card skirt uniform super make gloom';
+    test('Mnemonic of 12 words is valid', () {
+      expect(Mnemonic.isValid(mnemonic: mnemonic12Words), equals(true));
+    });
+
+    test('Mnemonic of 24 words is valid', () {
+      expect(Mnemonic.isValid(mnemonic: mnemonic24Words), equals(true));
+    });
+
+    test('Generated Mnemonic has 24 words', () {
+      expect(Mnemonic().generate.split(' ').length, equals(24));
+    });
+
+    test('Import 12 seed mnemonic', () {
+      expect(
+          Mnemonic.import(mnemonic: mnemonic12Words).mnemonic(),
+          equals(trust_core.HDWallet.createWithMnemonic(mnemonic12Words)
+              .mnemonic()));
+    });
+    test('Import 24 seed mnemonic', () {
+      expect(
+          Mnemonic.import(mnemonic: mnemonic24Words).mnemonic(),
+          equals(trust_core.HDWallet.createWithMnemonic(mnemonic24Words)
+              .mnemonic()));
     });
   });
-
-  // group('String', () {
-  //   test('.split() splits the string on the delimiter', () {
-  //     var string = 'foo,bar,baz';
-  //     expect(string.split(','), equals(['foo', 'bar', 'baz']));
-  //   });
-
-  //   test('.trim() removes surrounding whitespace', () {
-  //     var string = '  foo ';
-  //     expect(string.trim(), equals('foo'));
-  //   });
-  // });
 }
