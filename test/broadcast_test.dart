371aeaf96873467fa97d41ff40937f5c54a98b48
ciripel <nedelcu.alexandru@yahoo.com>
2022-03-24 18:16:48 +0200

    fix: removed comments in broadcast

--- test/broadcast_test.dart
+++ test/broadcast_test.dart
@@ -14,32 +14,4 @@ void main() {
     expect(jsonDecode(response)["error"]["message"],
         equals('Transaction simulation failed: Blockhash not found'));
   });
-
-  // Following tests are just an example to be used as model
-  // group('String', () {
-  //   test('.split() splits the string on the delimiter', () {
-  //     var string = 'foo,bar,baz';
-  //     expect(string.split(','), equals(['foo', 'bar', 'baz']));
-  //   });
-
-  //   test('.trim() removes surrounding whitespace', () {
-  //     var string = '  foo ';
-  //     expect(string.trim(), equals('foo'));
-  //   });
-  // });
-
-  // group('int', () {
-  //   test('.remainder() returns the remainder of division', () {
-  //     expect(11.remainder(3), equals(2));
-  //   });
-
-  //   test('.toRadixString() returns a hex string', () {
-  //     expect(11.toRadixString(16), equals('b'));
-  //   });
-  // });
-
-  // test('.split() splits the string on the delimiter', () {
-  //   expect('foo,bar,baz',
-  //       allOf([contains('foo'), isNot(startsWith('bar')), endsWith('baz')]));
-  // });
 }
