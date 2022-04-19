6a26f5cecfdc4da8bcc6aab5e9863e20473660ff
ciripel <nedelcu.alexandru@yahoo.com>
2022-04-19 10:30:54 +0300

    fix: get_balance tests for blockbook

--- test/get_balance_test.dart
+++ test/get_balance_test.dart
@@ -16,7 +16,7 @@ void main() {
     });
     test('Bitcoin Cash', () async {
       const address = 'bitcoincash:qphjd0f4jeg9naf29u6tkakv800wgksyhvsamcpdd2';
-      final response = await GetBalance.bitcoin(
+      final response = await GetBalance.bitcoinCash(
         apiEndpoint: 'https://bch1.simplio.io/',
         address: address,
       );
@@ -25,7 +25,7 @@ void main() {
     });
     test('Dash', () async {
       const address = 'XgbeASTQTNgPuTyAWb8NVDWVDmhgKJrfRV';
-      final response = await GetBalance.bitcoin(
+      final response = await GetBalance.dash(
         apiEndpoint: 'https://dash1.simplio.io/',
         address: address,
       );
@@ -34,7 +34,7 @@ void main() {
     });
     test('DigiByte', () async {
       const address = 'dgb1qshqfysjmxpl0dv5ryjeg8k4tkdp2pfk6k0gvdq';
-      final response = await GetBalance.bitcoin(
+      final response = await GetBalance.digibyte(
         apiEndpoint: 'https://dgb1.simplio.io/',
         address: address,
       );
@@ -43,7 +43,7 @@ void main() {
     });
     test('Doge', () async {
       const address = 'DTbELQaWmv5KpFcNpZ9X9wy5RjQGL4YMm2';
-      final response = await GetBalance.bitcoin(
+      final response = await GetBalance.doge(
         apiEndpoint: 'https://doge1.simplio.io/',
         address: address,
       );
@@ -52,7 +52,7 @@ void main() {
     });
     // test('Flux', () async {
     //   const address = 't1amMB14YTcUktfjHrz42XcDb2tdHmjgMQd';
-    //   final response = await GetBalance.bitcoin(
+    //   final response = await GetBalance.flux(
     //     apiEndpoint: 'https://btc1.simplio.io/',
     //     address: address,
     //   );
@@ -61,7 +61,7 @@ void main() {
     // });
     test('Litecoin', () async {
       const address = 'ltc1q4jd8494yun73v5ul2wcl5p32lcxm66afx4efr6';
-      final response = await GetBalance.bitcoin(
+      final response = await GetBalance.litecoin(
         apiEndpoint: 'https://ltc1.simplio.io/',
         address: address,
       );
@@ -70,7 +70,7 @@ void main() {
     });
     test('Zcash', () async {
       const address = 't1RTNMRyJhc1UgfvrTSqFP46C5xuyxjJGeR';
-      final response = await GetBalance.bitcoin(
+      final response = await GetBalance.zcash(
         apiEndpoint: 'https://zec1.simplio.io/',
         address: address,
       );
