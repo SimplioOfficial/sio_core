1e1fb610ac76d3c63987df71efcf3c8e82d6b7cc
ciripel <nedelcu.alexandru@yahoo.com>
2022-05-24 16:29:33 +0300

    subtask: get smallest denomination for cosmos

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,22 +1,46 @@
 import 'package:convert/convert.dart';
 import 'package:sio_core/src/utils_internal.dart';
 
-/// Get the latest blockhash for solana transactions signing.
-/// Find more details at
-/// https://docs.solana.com/developing/clients/jsonrpc-api#getlatestblockhash
-Future<String> latestBlockHashRequest({
+/// Converts BigInt numbers into list of bytes.
+///
+/// Example:
+/// * `12345678910111213141516171819202122232425 is converted to
+/// [36, 71, 219, 69, 9, 146, 148, 96, 119, 231, 69, 190, 155, 124, 198, 50, 105]`
+List<int> bigIntToBytes(BigInt number) {
+  var inHex = number.toRadixString(16);
+  if (inHex.length % 2 == 1) {
+    inHex = '0' + inHex;
+  }
+  return hex.decode(inHex);
+}
+
+/// Get ATOM, LUNA, OSMO minimal denomination on mainnet.
+String cosmosDenomination({
+  required String ticker,
+}) {
+  if (ticker == 'ATOM') return 'uatom';
+  if (ticker == 'LUNA') return 'uluna';
+  if (ticker == 'OSMO') return 'uosmo';
+  throw Exception('coin TICKER is not supported');
+}
+
+/// Get the account details from a cosmos ecosystem address.
+/// The result can be parsed to get the account number and the sequence.
+///
+/// Example:
+/// * https://lcd-osmosis.keplr.app/cosmos/auth/v1beta1/accounts/osmo1rlwemt45ryzc8ynakzwgfkltm7jy8lswpnfswn
+/// ```
+/// final response = await getCosmosAccountDetails(
+///   address: 'osmo1rlwemt45ryzc8ynakzwgfkltm7jy8lswpnfswn',
+///   apiEndpoint: 'https://lcd-osmosis.keplr.app/',
+/// );
+/// ```
+Future<String> getCosmosAccountDetails({
+  required String address,
   required String apiEndpoint,
 }) async {
-  final request = await postEncodedRequest(apiEndpoint, {
-    "jsonrpc": "2.0",
-    "id": "1",
-    "method": "getLatestBlockhash",
-    "params": [
-      {
-        "commitment": "confirmed",
-      }
-    ]
-  });
+  final request =
+      await getRequest(apiEndpoint + 'cosmos/auth/v1beta1/accounts/' + address);
   return request.body;
 }
 
@@ -47,35 +71,21 @@ Future<String> getUtxo({
   return request.body;
 }
 
-/// Converts BigInt numbers into list of bytes.
-///
-/// Example:
-/// * `12345678910111213141516171819202122232425 is converted to
-/// [36, 71, 219, 69, 9, 146, 148, 96, 119, 231, 69, 190, 155, 124, 198, 50, 105]`
-List<int> bigIntToBytes(BigInt number) {
-  var inHex = number.toRadixString(16);
-  if (inHex.length % 2 == 1) {
-    inHex = '0' + inHex;
-  }
-  return hex.decode(inHex);
-}
-
-/// Get the account details from a cosmos ecosystem address.
-/// The result can be parsed to get the account number and the sequence.
-///
-/// Example:
-/// * https://lcd-osmosis.keplr.app/cosmos/auth/v1beta1/accounts/osmo1rlwemt45ryzc8ynakzwgfkltm7jy8lswpnfswn
-/// ```
-/// final response = await getCosmosAccountDetails(
-///   address: 'osmo1rlwemt45ryzc8ynakzwgfkltm7jy8lswpnfswn',
-///   apiEndpoint: 'https://lcd-osmosis.keplr.app/',
-/// );
-/// ```
-Future<String> getCosmosAccountDetails({
-  required String address,
+/// Get the latest blockhash for solana transactions signing.
+/// Find more details at
+/// https://docs.solana.com/developing/clients/jsonrpc-api#getlatestblockhash
+Future<String> latestBlockHashRequest({
   required String apiEndpoint,
 }) async {
-  final request =
-      await getRequest(apiEndpoint + 'cosmos/auth/v1beta1/accounts/' + address);
+  final request = await postEncodedRequest(apiEndpoint, {
+    "jsonrpc": "2.0",
+    "id": "1",
+    "method": "getLatestBlockhash",
+    "params": [
+      {
+        "commitment": "confirmed",
+      }
+    ]
+  });
   return request.body;
 }
