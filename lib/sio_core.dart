57d3c0fac8120a9f8eeeb8d3d0d4eda0e1238b61
ciripel <nedelcu.alexandru@yahoo.com>
2022-06-03 19:50:02 +0300

    fix: exposed get_decimals

--- lib/sio_core.dart
+++ lib/sio_core.dart
@@ -1,7 +1,8 @@
-export 'src/build_transaction.dart';
 export 'src/broadcast.dart';
+export 'src/build_transaction.dart';
 export 'src/exceptions.dart';
 export 'src/get_balance.dart';
+export 'src/get_decimals.dart';
 export 'src/get_transactions.dart';
 export 'src/mnemonic.dart';
 export 'src/utils.dart';
